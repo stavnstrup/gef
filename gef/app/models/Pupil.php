bd6838864908ee6f8214a4180079a1f16596c6c9
Jens Stavnstrup <stavnstrup@gmail.com>
2014-09-11 16:33:12 +0200

    Add models

---
+++ gef/app/models/Pupil.php
@@ -0,0 +1,8 @@
+<?php
+
+// app/models/Pupil.php
+
+class Pupil extends Eloquent {
+      // let eloquent know that these attributes will be availible for mass assignment
+      protected $fillable = array();
+}
---
+++ gef/app/models/Workshop.php
@@ -0,0 +1,8 @@
+<?php
+
+// app/models/Workshop.php
+
+class Workshop extends Eloquent {
+      // let eloquent know that these attributes will be availible for mass assignment
+      protected $fillable = array('name', 'description', 'teaser_image', 'freeplaces');
+}
