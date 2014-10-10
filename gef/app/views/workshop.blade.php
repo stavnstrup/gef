847cde38717a4f0729303bba26c1306cb9859fdb
Jens Stavnstrup <stavnstrup@gmail.com>
2014-10-10 14:55:33 +0200

    Make workshop look nice on phones and tablets

--- gef/app/views/workshop.blade.php
+++ gef/app/views/workshop.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 
   <div class="row">
-    <div class="small-12 columns">
+    <div class="small-12 medium-12 large-12 columns">
 
       <div class="wsheader c{{$ws->id % 6}}">
        
