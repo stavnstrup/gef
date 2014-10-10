ef81b7af6a77b426192af6960b3f2f470f3b33ff
Jens Stavnstrup <stavnstrup@gmail.com>
2014-10-10 15:00:07 +0200

    Show workshop header image only on large+ media

--- gef/app/views/workshop.blade.php
+++ gef/app/views/workshop.blade.php
@@ -7,7 +7,7 @@
   <div class="row">
     <div class="small-12 medium-12 large-12 columns">
 
-      <div class="wsheader c{{$ws->id % 6}}">
+      <div class="wsheader show-for-large-up c{{$ws->id % 6}}">
        
       </div>
       <h2 class="subheader">{{$ws->id}}: {{$ws->title}}</h2>
