44bee45555fa2e3c43018f39c5884858f97778a7
Jens Stavnstrup <stavnstrup@gmail.com>
2015-10-01 09:26:50 +0200

    wstype er en imutable attribut

--- gef/app/models/Workshop.php
+++ gef/app/models/Workshop.php
@@ -4,7 +4,7 @@
 
 class Workshop extends Eloquent {
       // let eloquent know that these attributes will be availible for mass assignment
-      protected $fillable = array('title', 'subtitle', 'description', 'teaser_image', 'freeplaces', 'uri');
+      protected $fillable = array('title', 'subtitle', 'description', 'teaser_image', 'freeplaces', 'uri', 'wstype');
 
       public function pupil() {
       	     return $this->belongsTo('Pupil');
